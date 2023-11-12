0e52952c3aabddf37aa074ab0bad447b57a5f466
Anthony Ciccarello <aciccarello@sitepen.com>
2023-11-11 20:05:32 -0800

    add cloudflared tunnel to start script

--- start.sh
+++ start.sh
@@ -4,5 +4,22 @@ cd "$(dirname "$0")"
 
 # Make sure everything is up to date so don't need to log in to machine to update
 git pull
+
+# Make sure cloudflare tunnel is up
+if pgrep -x cloudflared > /dev/null
+then
+  echo "Tunnel already running"
+else
+  if command -v cloudflared &> /dev/null
+  then
+    echo "Starting Cloudflare tunnel"
+    cloudflared tunnel run indiekit &
+  else
+    # Don't expect to have cloudflared tunnel on local machine
+    echo "Unable to find cloudflared command. Skipping tunnel"
+  fi
+fi
+
+# Start indiekit
 npm install
 npm start
